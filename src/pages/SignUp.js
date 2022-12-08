bf3dc0a7fa160e2f7b1d5db105551784953040f2
Edwin Wanjuki <edwinwanjuki@gmail.com>
2022-12-08 07:39:42 +0300

    sign in & sign up forms

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,90 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const changeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div>Sign Up</div>
-  )
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Welcome Back!</p>
+      </header>
+
+      <form>
+        <input
+          type="text"
+          className="nameInput"
+          placeholder="Name"
+          id="name"
+          value={name}
+          onChange={changeHandler}
+        />
+
+        <input
+          type="email"
+          className="emailInput"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={changeHandler}
+        />
+
+        <div className="passwordInputDiv">
+          <input
+            type={showPassword ? "text" : "password"}
+            className="passwordInput"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={changeHandler}
+          />
+
+          <img
+            src={visibilityIcon}
+            alt="show password"
+            className="showPassword"
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          />
+        </div>
+
+        <Link to="/forgot-password" className="forgotPasswordLink">
+          Forgot Password
+        </Link>
+
+        <div className="signUpBar">
+          <p className="signUpText">Sign Up</p>
+          <button className="signUpButton">
+            <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+          </button>
+        </div>
+      </form>
+
+      {/* Google Auth */}
+
+      <Link to="/sign-in" className="registerLink">
+        Sign In Instead
+      </Link>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
