5b36c5b0d4b6f8cbe11a67afefd3de376f30d284
Edwin Wanjuki <edwinwanjuki@gmail.com>
2022-12-16 17:13:30 +0300

    sign in user

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    setUser(auth.currentUser);
+  });
+
+  return user ? <h1>{user.displayName}</h1> : "Not Logged In";
 }
 
-export default Profile
+export default Profile;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
@@ -22,13 +23,33 @@ function SignIn() {
     }));
   };
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="pageContainer">
       <header>
         <p className="pageHeader">Welcome Back!</p>
       </header>
 
-      <form>
+      <form onSubmit={submitHandler}>
         <input
           type="email"
           className="emailInput"
