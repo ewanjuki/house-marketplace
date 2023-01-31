7716c2ba527a1cf69d223013d43108b8c3da1a00
Edwin Wanjuki <edwinwanjuki@gmail.com>
2023-01-31 05:22:00 +0300

    getting coordinates with or without the geocoding api

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 import Spinner from "../components/Spinner";
 
 function CreateListing() {
@@ -51,6 +53,12 @@ function CreateListing() {
     });
   }, []);
 
+  useEffect(() => {
+    if (process.env.REACT_APP_GEOCODE_API_KEY !== "") {
+      setGeolocationEnabled(true);
+    }
+  }, []);
+
   const mutateHandler = (e) => {
     let boolean;
 
@@ -79,9 +87,55 @@ function CreateListing() {
     }
   };
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(formData);
+
+    setLoading(true);
+
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price needs to be less than regular price");
+      return;
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Max 6 images");
+      return;
+    }
+
+    let geolocation = {};
+    let location;
+
+    if (geolocationEnabled) {
+      // Call Google Geocoding API, if apiKey is enabled.
+      const apiKey = "";
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+
+      const data = await response.json();
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : data.results[0]?.formatted_address;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+      location = address;
+    }
+
+    setLoading(false);
   };
 
   if (loading) {
